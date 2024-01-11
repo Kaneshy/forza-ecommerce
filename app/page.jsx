60ef34d9f62e706c357959df1ad865b4468dc4f6
david <kaneshiuchiha@gmail.com>
2024-01-10 20:45:56 -0500

    carousel added

--- app/page.jsx
+++ app/page.jsx
@@ -1,12 +1,15 @@
+
+import Carousel from "@/components/Carousel";
 import Navbar from "@/components/bars/Navbar";
 import Link from "next/link";
+import { Suspense } from "react";
 
 export default function Home() {
   return (
     <main >
 
       <section className="home-widge-a h-screen flex flex-col">
-        <Navbar value={'text-white hover:bg-white hover:text-black'}/>
+        <Navbar value={'text-white hover:bg-white hover:text-black'} />
         <div className="absolute max-w-96 flex flex-col gap-y-4 bottom-20 left-20 p-4 text-white">
           <h1>Para Regalar</h1>
           <p>Descubre los regalos especialmente seleccionados para ti y tus seres queridos.</p>
@@ -14,42 +17,47 @@ export default function Home() {
         </div>
 
       </section>
-      <section className="flex min-h-screen gap-2 p-16 max-lg:flex-col">
+      <section className="flex min-h-screen gap-2 p-16 max-lg:flex-col select-none">
         <div className=" relative flex bg-img-woman h-screen">
           <div className="absolute bottom-10 right-10  flex flex-col gap-2">
-            <h1 className="text-white text-end">WOMAN</h1>
-            <Link className="p-4 text-black bg-white" href={'/'}>EXPLORE THE COLLECTION</Link>
+            <h1 className="text-white text-end">EXPLORE THE COLLECTION</h1>
+            <Link href={'/filterby/bygender/female'} className="p-4 text-black bg-white text-center" >WOMAN</Link>
           </div>
         </div>
         <div className="relative flex  bg-img-man h-screen">
           <div className="absolute bottom-10  right-10 flex flex-col gap-2">
-            <h1 className="text-white text-end">MAN</h1>
-            <Link className="p-4 text-black bg-white" href={'/'}>EXPLORE THE COLLECTION</Link>
+            <h1 className="text-white text-end">EXPLORE THE COLLECTION</h1>
+            <Link href={'/filterby/bygender/male'} className="p-4 text-black bg-white text-center" >MAN</Link>
           </div>
         </div>
       </section>
       <section className="flex min-h-screen bg-black gap-2 p-16 max-lg:flex-col">
         <div className=" relative flex bg-img-acs1 h-screen">
           <div className="absolute bottom-10 right-10  flex flex-col gap-2">
-            <h1 className="text-white text-end">WOMAN</h1>
-            <Link className="p-4 text-black bg-white" href={'/'}>EXPLORE THE COLLECTION</Link>
+            <h1 className="text-white text-end"> EXPLORE THE COLLECTION</h1>
+            <Link href={'/filterby/bygender/female'} className="p-4 text-black bg-white text-center" >KIDS</Link>
           </div>
         </div>
         <div className="relative flex  bg-img-acs2 h-screen">
           <div className="absolute bottom-10  right-10 flex flex-col gap-2">
-            <h1 className="text-white text-end">MAN</h1>
-            <Link className="p-4 text-black bg-white" href={'/'}>EXPLORE THE COLLECTION</Link>
+            <h1 className="text-white text-end"> EXPLORE THE COLLECTION</h1>
+            <Link href={'/filterby/bygender/accessories'} className="p-4 text-black bg-white text-center" >ACCESSORIES</Link>
           </div>
         </div>
         <div className="relative flex  bg-img-acs3 h-screen">
           <div className="absolute bottom-10  right-10 flex flex-col gap-2">
-            <h1 className="text-white text-end">MAN</h1>
-            <Link className="p-4 text-black bg-white" href={'/'}>EXPLORE THE COLLECTION</Link>
+            <h1 className="text-white text-end">EXPLORE THE COLLECTION</h1>
+            <Link href={'/filterby/bygender/jewelry'} className="p-4 text-black bg-white text-center">JEWELRY</Link>
           </div>
         </div>
       </section>
-      <section className="flex min-h-screen bg-white gap-2 p-16 max-lg:flex-col">
-
+      <section className="flex  text-black gap-2 p-16 max-sm:p-0 max-lg:flex-col">
+        <Suspense>
+          <div className=" w-full py-20">
+            <h1 className="text-3xl font-bold mb-4 text-center">Best Seller</h1>
+            <Carousel />
+          </div>
+        </Suspense>
       </section>
     </main>
   )
---
+++ components/Carousel.jsx
@@ -0,0 +1,66 @@
+'use client'
+import React, { useEffect, useState } from 'react';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import axios from 'axios';
+
+
+
+const Carousel = () => {
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 5,
+        slidesToScroll: 2,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: true,
+                },
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    initialSlide: 1,
+                },
+            },
+        ],
+    };
+
+    const [products, setproducts] = useState([])
+    const [filterH, setfilterH] = useState(false)
+    
+    
+    
+    useEffect(() => {
+      const dataproducts = async () => {
+        const res = await axios.get(`/api/bygender/jewelry`)
+        setproducts(res.data)
+      }
+      dataproducts()
+    }, [])
+
+    return (
+        <Slider {...settings} >
+
+            {products.map((x) => {
+                return (
+                    <div className='border-2 h-96 flex object-cover' >
+                        <img className='flex w-full object-cover'  src={x.imgUrl} alt="" />
+                    </div>
+                )
+            })}
+
+        </Slider>
+    );
+};
+
+export default Carousel;
